dcee19a52b45dea9307824e4e14056f63fcfa4ce
Prince Christiane Tolentino <princechristianetolentino@Princes-MacBook-Air.local>
2026-03-27 19:12:09 +0800

    fdsa

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -20,7 +20,9 @@ const PRODUCTS = {
     'Flexible Plan with AI-Add On and Third-Party Integration': { amount: 20800, currency: 'PHP' },
     'Flexible Plan with Third-Party Integration': { amount: 12800, currency: 'PHP' },
     'Premium Plan': { amount: 10000, currency: 'PHP' },
-    'Premium Plan with AI-Add On': { amount: 19200, currency: 'PHP' }
+    'Premium Plan with AI-Add On': { amount: 19200, currency: 'PHP' },
+    'Premium Plan with AI-Add On and Third Party Integration': { amount: 25000, currency: 'PHP' },
+    'Premium Plan with Third-Party Integration': { amount: 17000, currency: 'PHP' }
 };
 
 // Create payment intent
